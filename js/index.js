3f3a18d3677c8ab702f181f58d457554d7f5e47c
Ignacio <ignacio.revainera@hotmail.com>
2023-11-08 23:25:25 -0300

    se modificaron las rutas de los enlaces del header y footer para que dirijan a las páginas correctas, se corrigieron nombres de imágenes y se agregaron tamaños de fuente responsive

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const Audi = document.getElementById("concesionariaAudi");
+const Maserati = document.getElementById("concesionariaMaserati")
+const McLaren = document.getElementById("concesionariaMcLaren")
+
+const mapaAudi = document.getElementById("mapaConcesionariaAudi")
+const mapaMaserati = document.getElementById("mapaConcesionariaMaserati")
+const mapaMcLaren = document.getElementById("mapaConcesionariaMcLaren")
+
+const banderaArgentina = document.getElementById("banderaArgentina")
+const banderaItalia = document.getElementById("banderaItalia")
+const banderaFrancia = document.getElementById("banderaFrancia")
+
+Audi.onclick = () => {
+    mapaAudi.classList.add("visible")
+    mapaMaserati.classList.remove("visible")
+    mapaMcLaren.classList.remove("visible")
+    banderaArgentina.classList.add("activo")
+    banderaItalia.classList.remove("activo")
+    banderaFrancia.classList.remove("activo")
+}
+Maserati.onclick = () => {
+    mapaAudi.classList.remove("visible")
+    mapaMaserati.classList.add("visible")
+    mapaMcLaren.classList.remove("visible")
+    banderaArgentina.classList.remove("activo")
+    banderaItalia.classList.add("activo")
+    banderaFrancia.classList.remove("activo")
+}
+McLaren.onclick = () => {
+    mapaAudi.classList.remove("visible")
+    mapaMaserati.classList.remove("visible")
+    mapaMcLaren.classList.add("visible")
+    banderaArgentina.classList.remove("activo")
+    banderaItalia.classList.remove("activo")
+    banderaFrancia.classList.add("activo")
+}
